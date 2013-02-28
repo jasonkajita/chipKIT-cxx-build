35eed656182c75c71579b7b0bcf70b61f6ad54e1
Jason Kajita <jason@jasonkajita.com>
2013-02-28 09:31:38 -0700

    Make sure that we call strip on the Windows executables

--- chipKIT-cxx-build.sh
+++ chipKIT-cxx-build.sh
@@ -1096,7 +1096,7 @@ unset CPP
 unset CXX
 
 cd $WORKING_DIR/win32-image/pic32-tools
-#find . -type f -name "*.exe" | xargs $MINGW_HOST_PREFIX-strip
+find . -type f -name "*.exe" | xargs $MINGW_HOST_PREFIX-strip
 
 status_update "Make minGW32 Canadian cross build complete"
 
